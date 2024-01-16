09dafa59c49025261da19ab8c03b3ef600bb80eb
Dokkae6949 <finnliry@gmail.com>
2024-01-16 19:09:59 +0100

    Do not let home-manager manage ags config

--- home/pkgs/apps/shells/ags/default.nix
+++ home/pkgs/apps/shells/ags/default.nix
@@ -7,7 +7,7 @@
     enable = true;
 
     # null or path, leave as null if you don't want hm to manage the config
-    configDir = ./config;
+    configDir = null;
 
     # additional packages to add to gjs's runtime
     extraPackages = [ pkgs.libsoup_3 ];
