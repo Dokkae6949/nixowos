18f25f7d03d35b95dcf65459223697b3af3392ba
Dokkae6949 <finnliry@gmail.com>
2024-03-20 11:42:32 +0100

    Enable nvidia power management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -304,7 +304,7 @@ in
 
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = false;
+      powerManagement.enable = true;
       powerManagement.finegrained = false;
       open = false;
 
