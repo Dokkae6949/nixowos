e8260e6d1f936be32461416111d918fe8db31d23
Dokkae6949 <finnliry@gmail.com>
2024-07-27 16:58:40 +0200

    Add gnome polkit

--- nixos/evergarden/configuration/security.nix
+++ nixos/evergarden/configuration/security.nix
@@ -3,6 +3,7 @@
 {
   security = {
     rtkit.enable = true;
+    polkit.enable = true;
     pam.services.swaylock = {};
   };
   
@@ -13,5 +14,25 @@
     };
 
     gnome.gnome-keyring.enable = true;
+  };
+
+  environment.systemPacakges = with pkgs; [
+    polkit_gnome
+  ];
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
   };
 }
