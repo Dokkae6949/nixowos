19c8a62f6e13c516630aaa012dfe5ea493698e63
Dokkae6949 <finnliry@gmail.com>
2025-01-04 12:24:40 +0000

    Change zen browser flake

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
     };
 
     zen-browser = {
-      url = "github:Dokkae6949/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
