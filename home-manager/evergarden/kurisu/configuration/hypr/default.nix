73d2b44b304787b59d618adade64aaafb29f69a3
Dokkae6949 <finnliry@gmail.com>
2025-01-22 18:00:38 +0000

    Change NVD_BACKEND emv

--- home-manager/evergarden/kurisu/configuration/hypr/default.nix
+++ home-manager/evergarden/kurisu/configuration/hypr/default.nix
@@ -21,6 +21,7 @@ in
       XDG_SESSION_TYPE = "wayland";
       GBM_BACKEND = "nvidia-drm";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      NVD_BACKEND = "direct";
 
       XCURSOR_THEME = cursor-theme-name;
       HYPRCURSOR_THEME = cursor-theme-name;
